74543efd856991b5f4f5977d6a4042c5a5e6fd79
Kyle J <kajeffr1@asu.edu>
2018-10-19 00:56:03 -0400

    Always Improving!

--- JeffreysScheduler/main.c
+++ JeffreysScheduler/main.c
@@ -1,134 +1,51 @@
 /**
- * Program that emulates interaction with courses
- * (e.g. Adding, deleting, viewing schedule)
- *
- * Completion time: (~2 hours)
- *
- * @author (Kyle Jeffreys), (Ruben Acuna)
- * @version (1.0)
- */
+* This program represents a scheduler for college students.
+*
+* Completion time: (~4 hours)
+*
+* @author (Kyle Jeffreys), (Ruben Acuna)
+* @version (1.0)
+*/
 
 ////////////////////////////////////////////////////////////////////////////////
 //INCLUDES
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 ////////////////////////////////////////////////////////////////////////////////
 //DATA STRUCTURES
-
 typedef enum {
-    SER, EGR, CSE, EEE
+    SER,
+    EGR,
+    CSE,
+    EEE,
 } Subject;
 
+
+// Structure of a course node
 typedef struct CourseNode {
     Subject subject;
-    int courseNumber;
+    int courseCode;
     char teacher[1024];
     int creditHours;
+    // this stores the address of next course node in the linked list
     struct CourseNode *next;
 } CourseNode;
 
-
 ////////////////////////////////////////////////////////////////////////////////
 //GLOBAL VARIABLES
-
+int subjectCode;
+    int courseNumber;
+    int credits;
+    char teacher[1024];
 //place to store course information
-CourseNode *course_collection = NULL;
-
-int subjectEnum;
-int courseNumber;
-char teacher[1024];
-int creditHours;
+struct CourseNode *course_collection = NULL;
 
 
-////////////////////////////////////////////////////////////////////////////////
-//FORWARD DECLARATIONS
-void branching(char option);
-const char* getSubject(Subject subject);
-int totalCredits(struct CourseNode *head);
-void course_insert(struct CourseNode** head, int subjectEnum, int courseNumber,
-	int creditHours, char* teacher);
-
-//main entry point. Starts the program by displaying a welcome and beginning an 
-//input loop that displays a menu and processes user input. Pressing q quits.      
-
-int main() {
-    char input_buffer;
-	
-
-    printf("\n\nWelcome to ASU Class Schedule\n");
-
-    //menu and input loop
-    do {
-        printf("\nMenu Options\n");
-        printf("------------------------------------------------------\n");
-        printf("a: Add a class\n");
-        printf("d: Drop a class\n");
-        printf("s: Show your classes\n");
-        printf("q: Quit\n");
-        //printf("\nTotal Credits: %d\n\n", totalCredits(course_collection));
-        printf("Please enter a choice ---> ");
-
-        scanf(" %c", &input_buffer);
-
-        branching(input_buffer);
-    } while (input_buffer != 'q');
-
-    //free(course_collection);
-    return 0;
-}
-
-//takes a character representing an inputs menu choice and calls the appropriate
-//function to fulfill that choice. display an error message if the character is
-//not recognized.
-
-void branching(char option) {
-    switch (option) {
-        case 'a':
-            printf("What is the subject? (SER=0, EGR=1, CSE=2, EEE=3)?\n");
-            scanf("%d", &subjectEnum); 
-            printf("\n");
-			
-            printf("What is the number (e.g. 240)?\n");
-            scanf("%d", &courseNumber);
-            printf("\n");
-			
-            printf("How many credits is the class (e.g. 3)?\n");
-            scanf("%d", &creditHours);
-            printf("\n");
-			
-            printf("What is the name of the teacher?\n");
-            scanf("%s", teacher);
-            printf("\n");
-            // printf("%d %d %d %s", subjectEnum, courseNumber, creditHours, teacher);
-
-            course_insert(&course_collection, subjectEnum, courseNumber, creditHours,
-                    teacher);
-            break;
-
-        case 'd':
-            printf("Enter course number to delete: ");
-            scanf("%d", &courseNumber);
-            course_drop(&course_collection, courseNumber);
-            break;
-
-        case 's':
-            schedule_print(course_collection);
-            break;
-
-        case 'q':
-            // main loop will take care of this.
-            break;
-
-        default:
-            printf("\nError: Invalid Input.  Please try again...");
-            break;
-    }
-}
-
-//Using a switch statement to map
-//from an enumeration to a printf
-char* getSubject(Subject subject)
+// Use a switch statement to map
+// from an enumeration to a printf
+const char* getSubject(Subject subject)
 {
    switch (subject)
    {
@@ -150,27 +67,26 @@ int totalCredits(struct CourseNode *head)
     struct CourseNode *temp = head;
     while(temp != NULL)
     {
-        totalCredits += temp->creditHours;
+        totalCredits = totalCredits + temp->creditHours;
         temp = temp->next;
     }
     return totalCredits;
 }
 
 // insert course into linked list
-void course_insert(struct CourseNode** head, int subjectEnum, int courseNumber, 
-int creditHours, char *teacher){
+void course_insert(struct CourseNode** head, int subjectCode, int courseNumber, int credits, char *teacher){
 
     struct CourseNode *newNode, *current;
     newNode = (struct CourseNode*)malloc(sizeof(struct CourseNode));
 
-    newNode->subject = subjectEnum;
-    newNode->courseNumber = courseNumber;
+    newNode->subject = subjectCode;
+    newNode->courseCode = courseNumber;
     // newNode->teacher = malloc(1024);
     strcpy(newNode->teacher, teacher);
-    newNode->creditHours = creditHours;
+    newNode->creditHours = credits;
 
     // special case to insert data at head
-    if (*head == NULL || (*head)->courseNumber >= newNode->courseNumber)
+    if (*head == NULL || (*head)->courseCode >= newNode->courseCode)
     {
         newNode->next = *head;
         *head = newNode;
@@ -180,7 +96,7 @@ int creditHours, char *teacher){
         // Locate node before the point of insertion
         current = *head;
         while (current->next!=NULL &&
-               current->next->courseNumber < newNode->courseNumber)
+               current->next->courseCode < newNode->courseCode)
         {
             current = current->next;
         }
@@ -195,7 +111,7 @@ void course_drop(struct CourseNode **head, int courseNumber){
     struct CourseNode* temp = *head, *prev;
 
     // If head node itself holds the courseNumber to be deleted
-    if (temp != NULL && temp->courseNumber == courseNumber)
+    if (temp != NULL && temp->courseCode == courseNumber)
     {
         // head is changed
         *head = temp->next;
@@ -204,7 +120,7 @@ void course_drop(struct CourseNode **head, int courseNumber){
     }
   
     // keep track of previous node and search for the course to be deleted
-    while (temp != NULL && temp->courseNumber != courseNumber)
+    while (temp != NULL && temp->courseCode != courseNumber)
     {
         prev = temp;
         temp = temp->next;
@@ -222,15 +138,89 @@ void course_drop(struct CourseNode **head, int courseNumber){
     return;
 }
 
+
 void schedule_print(struct CourseNode *head){
 
     struct CourseNode *temp = head;
     printf("Class Schedule:\n");
     while(temp != NULL)
     {
-        printf("%s%d\t%d\t%s\n", getSubject(temp->subject), 
-			temp->courseNumber, temp->creditHours, temp->teacher);
+        printf("%s%d\t%d\t%s\n", getSubject(temp->subject), temp->courseCode, temp->creditHours, temp->teacher);
         temp = temp->next;
     }
 }
 
+
+int main(void) {
+	char input_buffer;
+	
+    
+    // head = (struct CourseNode*)malloc(sizeof(struct CourseNode));
+
+    do{
+
+
+        printf("\n\n");
+        printf("Welcome to ASU Class Schedule\n\n");
+        printf("Menu Options\n");
+        printf("----------------------------------\n");
+        printf("a: Add a class\n");
+        printf("d: Drop a class\n");
+        printf("s: Show your classes\n");
+        printf("q: Quit\n\n");
+
+        printf("Total credits: %d\n\n", totalCredits(course_collection));
+        printf("Please enter a choice ---> ");
+        scanf(" %c", &input_buffer);
+		branching(input_buffer);
+		
+	}	while(input_buffer != 'q');
+      
+        
+    // no memory leaks at the end
+    free(course_collection);
+    return 0;
+}
+
+void branching(char option)
+{
+	switch (option){
+
+            case 'a':
+                do{
+                    printf("What is the subject? (SER=0, EGR=1, CSE=2, EEE=3)?\n");
+                    scanf("%d", &subjectCode);
+                    if (subjectCode <0 || subjectCode > 3)
+                        printf("Enter number in range 0-3\n");
+                    else
+                        break;
+                }while(1);
+                printf("\n");
+                printf("What is the number (e.g. 240)?\n");
+                scanf("%d", &courseNumber);
+                printf("\n"); printf("How many credits is the class (e.g. 3)?\n");
+                scanf("%d", &credits);
+                printf("\n"); printf("What is the name of the teacher?\n");
+                scanf("%s", teacher);
+                printf("\n");
+                // printf("%d %d %d %s", subjectCode, courseNumber, credits, teacher);
+
+                course_insert(&course_collection, subjectCode, courseNumber, credits, teacher);
+
+                break;
+            case 'd':
+                printf("Enter course number to delete: ");
+                scanf("%d", &courseNumber);
+                course_drop(&course_collection, courseNumber);
+                break;
+            case 's':
+                schedule_print(course_collection);
+                break;
+            case 'q':
+                printf(" Quitting the program\n\n");
+                break;
+            default:
+                printf("ERROR! No such choice. PleaseEnter a valid choice.\n");
+                break;
+    }
+}
